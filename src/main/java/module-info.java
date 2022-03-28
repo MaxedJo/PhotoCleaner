138d244cfa30af6cbcae5d6dad95f27ef06679e6
MaxedJo <maxim.a.vorobyev@gmail.com>
2022-03-28 21:26:17 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module ru.maxed.photocleaner {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens ru.maxed.photocleaner to javafx.fxml;
+    exports ru.maxed.photocleaner;
+}
